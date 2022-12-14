957c7c8a3406d03aeff1bb7ebd7686b79c9bd4cd
Aurelien Meunier <me@aurelienmeunier.com>
2022-12-14 21:47:12 +0100

    day 12 part 2

--- day-12.py
+++ day-12.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from common import parse
 
@@ -43,7 +44,7 @@ def show_map(map, visited, start, end, x):
             elif ((i, j) == end):
                 line += "E"
             elif ((i, j) == x):
-                line += "x"
+                line += "O"
             elif visited[i][j]:
                 line += map[i][j]
             else:
@@ -51,8 +52,21 @@ def show_map(map, visited, start, end, x):
         print(line)
 
 
-def find_path(map):
+def reverse_map(map):
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] in ["S", "a"]:
+                map[i][j] = "E"
+            elif map[i][j] == "E":
+                map[i][j] = "S"
+            else:
+                map[i][j] = chr(ord("z") + ord("a") - ord(map[i][j]))
+
+
+def find_path(map, reverse=False):
     # Initialize
+    if reverse:
+        reverse_map(map)
     visited = []
     start, end = None, None
     for i in range(len(map)):
@@ -66,12 +80,12 @@ def find_path(map):
     while stack:
         state = stack.pop(0)
         pos = state.pos
-        if not debug:
-            show_map(map, visited, start, end, pos)
-        letter = map[pos[0]][pos[1]]
         if debug:
-            print(f"Looking at {pos}: {letter}. Stack={len(stack)}")
-        if pos == end:
+            show_map(map, visited, start, end, pos)
+            print(pos)
+            # time.sleep(1)
+        letter = map[pos[0]][pos[1]]
+        if not reverse and letter == "E":
             return state
         for d in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             next_pos = (pos[0] + d[0], pos[1] + d[1])
@@ -79,23 +93,20 @@ def find_path(map):
                 continue
             next_letter = map[next_pos[0]][next_pos[1]]
             if next_letter == "E":
-                if letter == "z":
+                if (reverse and letter == "y") or (not reverse and letter == "z"):
                     return state.go_to("E", next_pos)
-            elif letter == "S" or ((ord(next_letter) - ord(letter)) <= 1):
+            elif (letter == "S" and next_letter == "a") or ((ord(next_letter) - ord(letter)) <= 1):
                 inserted = False
                 next_state = state.go_to(next_letter, next_pos)
                 visited[next_pos[0]][next_pos[1]] = True
-                if debug:
-                    print(f"Adding {next_letter} at {next_pos} to stack.")
                 for i in range(len(stack)):
                     s = stack[i]
-                    if s.length > state.length:
+                    if (s.length > state.length):
                         stack.insert(i, next_state)
                         inserted = True
                         break
                 if not inserted:
                     stack.append(next_state)
-    print("=(")
 
 
 final_path = find_path(load_map(True))
@@ -103,3 +114,16 @@ assert(len(final_path.path) == 31)
 
 final_path = find_path(load_map(False))
 print(f"Part 1 - Final path length={len(final_path.path)}")
+
+map = load_map(True)
+final_path = find_path(map, reverse=True)
+assert(len(final_path.path) == 29)
+
+map = load_map(False)
+for i in range(len(map)):
+    line = ""
+    for j in range(len(map[i])):
+        line += map[i][j]
+    print(line)
+final_path = find_path(map, reverse=True)
+print(f"Part 2 - Final path length={len(final_path.path)}")
